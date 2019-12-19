cf57d52737b8cd30853461a5a30aad3afc936ac2
immteam <immteam@gmail.com>
2019-12-19 17:51:30 +0000

    Latest version uploaded to github

---
+++ RegexMastery/MainForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace RegexMastery {
+	partial class MainForm {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.RegexString = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.InputString = new System.Windows.Forms.TextBox();
+			this.ReplaceString = new System.Windows.Forms.TextBox();
+			this.OutputString = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.Exit = new System.Windows.Forms.Button();
+			this.IgnoreCase = new System.Windows.Forms.CheckBox();
+			this.SingleLine = new System.Windows.Forms.CheckBox();
+			this.MultiLine = new System.Windows.Forms.CheckBox();
+			this.Replace = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(101, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Regular Expression:";
+			// 
+			// RegexString
+			// 
+			this.RegexString.Font = new System.Drawing.Font("Consolas", 8.25F);
+			this.RegexString.Location = new System.Drawing.Point(121, 10);
+			this.RegexString.Name = "RegexString";
+			this.RegexString.Size = new System.Drawing.Size(495, 20);
+			this.RegexString.TabIndex = 1;
+			this.RegexString.TextChanged += new System.EventHandler(this.FormControl_Change);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(56, 63);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(58, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Input Text:";
+			// 
+			// InputString
+			// 
+			this.InputString.Font = new System.Drawing.Font("Consolas", 8.25F);
+			this.InputString.Location = new System.Drawing.Point(121, 60);
+			this.InputString.Multiline = true;
+			this.InputString.Name = "InputString";
+			this.InputString.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.InputString.Size = new System.Drawing.Size(495, 78);
+			this.InputString.TabIndex = 5;
+			this.InputString.TextChanged += new System.EventHandler(this.FormControl_Change);
+			// 
+			// ReplaceString
+			// 
+			this.ReplaceString.Font = new System.Drawing.Font("Consolas", 8.25F);
+			this.ReplaceString.Location = new System.Drawing.Point(121, 150);
+			this.ReplaceString.Name = "ReplaceString";
+			this.ReplaceString.Size = new System.Drawing.Size(495, 20);
+			this.ReplaceString.TabIndex = 7;
+			this.ReplaceString.TextChanged += new System.EventHandler(this.FormControl_Change);
+			// 
+			// OutputString
+			// 
+			this.OutputString.BackColor = System.Drawing.SystemColors.Window;
+			this.OutputString.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.OutputString.Location = new System.Drawing.Point(121, 180);
+			this.OutputString.Multiline = true;
+			this.OutputString.Name = "OutputString";
+			this.OutputString.ReadOnly = true;
+			this.OutputString.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.OutputString.Size = new System.Drawing.Size(495, 426);
+			this.OutputString.TabIndex = 8;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(48, 183);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(66, 13);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Output Text:";
+			// 
+			// Exit
+			// 
+			this.Exit.Location = new System.Drawing.Point(12, 583);
+			this.Exit.Name = "Exit";
+			this.Exit.Size = new System.Drawing.Size(95, 23);
+			this.Exit.TabIndex = 9;
+			this.Exit.Text = "Close";
+			this.Exit.UseVisualStyleBackColor = true;
+			this.Exit.Click += new System.EventHandler(this.Exit_Click);
+			// 
+			// IgnoreCase
+			// 
+			this.IgnoreCase.AutoSize = true;
+			this.IgnoreCase.Location = new System.Drawing.Point(121, 37);
+			this.IgnoreCase.Name = "IgnoreCase";
+			this.IgnoreCase.Size = new System.Drawing.Size(89, 17);
+			this.IgnoreCase.TabIndex = 2;
+			this.IgnoreCase.Text = "Ignore Case?";
+			this.IgnoreCase.UseVisualStyleBackColor = true;
+			this.IgnoreCase.Click += new System.EventHandler(this.FormControl_Change);
+			// 
+			// SingleLine
+			// 
+			this.SingleLine.AutoSize = true;
+			this.SingleLine.Location = new System.Drawing.Point(235, 37);
+			this.SingleLine.Name = "SingleLine";
+			this.SingleLine.Size = new System.Drawing.Size(84, 17);
+			this.SingleLine.TabIndex = 3;
+			this.SingleLine.Text = "Single Line?";
+			this.SingleLine.UseVisualStyleBackColor = true;
+			this.SingleLine.Click += new System.EventHandler(this.FormControl_Change);
+			// 
+			// MultiLine
+			// 
+			this.MultiLine.AutoSize = true;
+			this.MultiLine.Location = new System.Drawing.Point(346, 37);
+			this.MultiLine.Name = "MultiLine";
+			this.MultiLine.Size = new System.Drawing.Size(77, 17);
+			this.MultiLine.TabIndex = 4;
+			this.MultiLine.Text = "Multi Line?";
+			this.MultiLine.UseVisualStyleBackColor = true;
+			this.MultiLine.Click += new System.EventHandler(this.FormControl_Change);
+			// 
+			// Replace
+			// 
+			this.Replace.AutoSize = true;
+			this.Replace.Location = new System.Drawing.Point(24, 152);
+			this.Replace.Name = "Replace";
+			this.Replace.Size = new System.Drawing.Size(93, 17);
+			this.Replace.TabIndex = 6;
+			this.Replace.Text = "Replace Text:";
+			this.Replace.UseVisualStyleBackColor = true;
+			this.Replace.Click += new System.EventHandler(this.FormControl_Change);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(630, 618);
+			this.Controls.Add(this.Replace);
+			this.Controls.Add(this.MultiLine);
+			this.Controls.Add(this.SingleLine);
+			this.Controls.Add(this.IgnoreCase);
+			this.Controls.Add(this.Exit);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.OutputString);
+			this.Controls.Add(this.ReplaceString);
+			this.Controls.Add(this.InputString);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.RegexString);
+			this.Controls.Add(this.label1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(640, 650);
+			this.MinimumSize = new System.Drawing.Size(640, 650);
+			this.Name = "MainForm";
+			this.Text = "RegexMastery v1.0";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form_Close);
+			this.Shown += new System.EventHandler(this.Form_Show);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox RegexString;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox InputString;
+		private System.Windows.Forms.TextBox ReplaceString;
+		private System.Windows.Forms.TextBox OutputString;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button Exit;
+		private System.Windows.Forms.CheckBox IgnoreCase;
+		private System.Windows.Forms.CheckBox SingleLine;
+		private System.Windows.Forms.CheckBox MultiLine;
+		private System.Windows.Forms.CheckBox Replace;
+	}
+}
+
---
+++ RegexMastery/MainForm.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Utility.ModifyRegistry;
+
+namespace RegexMastery {
+	public partial class MainForm : Form {
+
+		public MainForm() {
+			InitializeComponent();
+		}
+
+		private void Exit_Click(object sender, EventArgs e) {
+			Serialize(); 
+			Close();
+		}
+
+		private void Form_Show(object sender, EventArgs e) {
+			LoadSerialization();
+		}
+
+		private void Form_Close(object sender, FormClosedEventArgs e) {
+			Serialize();
+		}
+
+		private void FormControl_Change(object sender, EventArgs e) {
+			Evaluate();
+		}
+
+		protected void Evaluate() {
+			string output = "";
+			string input = InputString.Text;
+			string pattern = RegexString.Text;
+			string replace = ReplaceString.Text;
+
+			try {
+				if (input != "" && pattern != "") {
+					Regex reg = new Regex(pattern, 
+						(IgnoreCase.Checked ? RegexOptions.IgnoreCase : RegexOptions.None) |
+						(SingleLine.Checked ? RegexOptions.Singleline : RegexOptions.None) |
+						(MultiLine.Checked ? RegexOptions.Multiline : RegexOptions.None)
+						);
+
+					if (Replace.Checked) {
+						output = reg.Replace(input, replace);
+					}
+					else {
+						int matchCount = 0;
+						MatchCollection matches = reg.Matches(input);
+						foreach (Match match in matches) {
+							output += string.Format("Regex.Matches[{0}] = {1}\r\n", matchCount, match.ToString() ?? "<null>");
+
+							int groupCount = 0;
+							GroupCollection groups = match.Groups;
+							foreach (Group group in groups) {
+								output += string.Format("     .Matches[{0}].Groups[{1}] = {2}\r\n", matchCount, groupCount++, group.ToString() ?? "<null>");
+							}
+
+							if (groupCount == 0) {
+								output += string.Format("     .Matches[{0}].Groups.Count = 0\r\n", matchCount);
+							}
+
+							output += "\r\n";
+							matchCount++;
+						}
+
+						if (matchCount == 0)
+							output += string.Format("Regex.Matches.Count = 0");
+					}
+				}
+				else
+					output = "Nothing to evaluate.";
+			}
+			catch (Exception e) {
+				output = "Exception Caught:\r\n";
+				while (e != null) {
+					output += e.Message + "\r\n";
+					e = e.InnerException;
+				}
+			}
+
+			OutputString.Text = output;
+		}
+
+		protected void Serialize() {
+			try {
+				ModifyRegistry reg = new ModifyRegistry();
+
+				reg.Write("Pattern", RegexString.Text);
+				reg.Write("Input", InputString.Text);
+				reg.Write("Replace", ReplaceString.Text);
+
+				reg.Write("IgnoreCase", IgnoreCase.Checked);
+				reg.Write("SingleLine", SingleLine.Checked);
+				reg.Write("MultiLine", MultiLine.Checked);
+				reg.Write("ReplaceOn", Replace.Checked);
+			}
+			catch (Exception ex) {
+				MessageBox.Show("Error Saving Form to Registry", ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		protected void LoadSerialization() {
+			try {
+				ModifyRegistry reg = new ModifyRegistry();
+
+				RegexString.Text = reg.Read("Pattern") ?? "";
+				InputString.Text = reg.Read("Input") ?? "";
+				ReplaceString.Text = reg.Read("Replace") ?? "";
+
+				IgnoreCase.Checked = (reg.Read("IgnoreCase") ?? "") == "True" ? true : false;
+				SingleLine.Checked = (reg.Read("SingleLine") ?? "") == "True" ? true : false;
+				MultiLine.Checked = (reg.Read("MultiLine") ?? "") == "True" ? true : false;
+				Replace.Checked = (reg.Read("ReplaceOn") ?? "") == "True" ? true : false;
+			}
+			catch (Exception ex) {
+				MessageBox.Show("Error Loading Form from Registry", ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+	}
+}
